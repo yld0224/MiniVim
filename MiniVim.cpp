a3b79c32f1e935c2e7c96b9b4ca8ae9ee0820b4a
yld0224 <yld0224@sjtu.edu.cn>
2026-07-19 20:25:05 +0800

    Feat: entering raw mode

--- MiniVim.cpp
+++ MiniVim.cpp
@@ -1,4 +1,61 @@
-int main(){
+/*** includes ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** data ***/
+termios ori_termios;
+
+/*** terminal ***/
+void die(const char *s) {
+	perror(s);
+	exit(1);
+}
+
+void disableRawMode(){
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &ori_termios) == -1){
+		die("tcsetattr");
+	}
+}
+
+void enableRawMode(){
+	if (tcgetattr(STDIN_FILENO, &ori_termios) == -1){
+		die("tcgetattr");
+	}
+	atexit(disableRawMode);
 	
+	termios raw = ori_termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
+		die("tcsetattr");
+	}
+}
+
+/*** init ***/
+int main(){
+	enableRawMode();
+
+	while (true){
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+			die("read");
+		}
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+		if (c == 'q') break;
+	}
+
 	return 0;
 }
